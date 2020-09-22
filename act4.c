ca8c4a14f3199759c9f3bedb0b9d6d8a8335ce41
Carlos Alcala <carlos.aceves2888@alumnos.udg.mx>
2020-09-22 18:44:49 -0500

    parte1 capturar numeros

--- act4.c
+++ act4.c
@@ -1,8 +1,54 @@
 #include<stdio.h>
 
-
+void capturar_enteros(){
+    int enteros[5];
+    float prom;
+    int suma=0;
+    for (size_t i = 0; i < 5; i++)
+    {
+        scanf("%i", &enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("%i\n", enteros[i]);
+    }
+    for (size_t i = 0; i < 5; i++)
+    {
+        suma = suma + enteros[i];
+    }
+    printf("suma: %i\n", suma);
+    printf("promedio: %.2f\n", suma/5.0);
+}
 
 int main(){
-   
+   char op;
+
+   do
+   {
+       printf("1) capturar enteros\n");
+       printf("2) mostrar cadena n veces\n");
+       printf("3) agrega personaje\n");
+       printf("4) salir\n");
+       scanf ("%c", &op);
+       
+        switch (op)
+        {
+        case '1':
+            capturar_enteros();
+            break;
+        case '2':
+
+            break;
+            case '3':
+
+            break;
+        default:
+            break;
+        }
+
+       fflush(stdin);
+   } while (op!='4');
+
+
     return 0;
 }
